3446b12de2588d39694a290eb597afb67b1a01f1
Xaver Kapeller <Wrdlbrnft@users.noreply.github.com>
2016-12-20 10:40:52 +0100

    Camera2 BarcodeReader is now only enabled for a few specific devices.

--- BetterBarcodes/src/main/java/com/github/wrdlbrnft/betterbarcodes/reader/BarcodeReaders.java
+++ BetterBarcodes/src/main/java/com/github/wrdlbrnft/betterbarcodes/reader/BarcodeReaders.java
@@ -17,9 +17,15 @@ import com.github.wrdlbrnft.proguardannotations.KeepClassMembers;
 @KeepClass
 @KeepClassMembers
 public class BarcodeReaders {
+    
+    private final List<String> CAMERA_2_DEVICES = Arrays.asList(
+            "nexus 4", "nexus 5", "nexus 5x", "nexus 6", "nexus 6p", "nexus 7", "nexus 10",
+            "gt-i9300", "samsung-sm-g925a", "samsung-sm-g935a", "samsung-sm-t817a", "sm-g900h",
+            "lgus991", "lg-h810", "xt1058", "aquaris e5", "c6602"
+    );
 
     public static BarcodeReader get(Context context, AspectRatioTextureView view) {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
+        return CAMERA_2_DEVICES.contains(Build.MODEL.toLowerCase())
                 ? new LollipopBarcodeReader(context, view)
                 : new IceCreamSandwichBarcodeReader(context, view);
     }
